59695c8fc53ba00e250e16f2e480f6e1c9fcbde0
Linda Saraguro <lindasaraguro@yahoo.com>
2019-07-31 18:19:22 -0400

    created models and associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -17,7 +17,6 @@ const Skill = require('./skill')
  * instead of: const User = require('../db/models/user')
  */
 Mentee.belongsTo(Mentor)
-Mentor.belongsTo(Mentee)
 Mentee.hasOne(Mentor)
 Skill.hasMany(Mentor)
 Mentor.hasMany(Skill)
