57bdf8e2f64ae206544eaba0d4f42a225f2f1bd4
sam-peach <sampeachart@gmail.com>
2019-08-01 11:49:56 -0400

    feat: built out signup form

--- client/store/user.js
+++ client/store/user.js
@@ -46,6 +46,32 @@ export const auth = (email, password, method) => async dispatch => {
   }
 }
 
+export const signup = (
+  email,
+  password,
+  mentorOrMentee,
+  skills
+) => async dispatch => {
+  let res
+  try {
+    res = await axios.post('/auth/signup', {
+      email,
+      password,
+      mentorOrMentee,
+      skills
+    })
+  } catch (authError) {
+    return dispatch(getUser({error: authError}))
+  }
+
+  try {
+    dispatch(getUser(res.data))
+    history.push('/home')
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
 export const logout = () => async dispatch => {
   try {
     await axios.post('/auth/logout')
