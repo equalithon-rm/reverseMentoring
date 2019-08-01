6372b0bc869f60e3553c4d1cb95e8d5fdc82da98
Linda Saraguro <lindasaraguro@yahoo.com>
2019-08-01 11:09:46 -0400

    created mentor models

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/mentors', require('./mentors'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
