803488c7be70cfb46e04c33ccfae6f07db837be0
Linda Saraguro <lindasaraguro@yahoo.com>
2019-08-01 12:31:59 -0400

    fix: renamed attributes for mentor and mentees

--- server/api/mentees.js
+++ server/api/mentees.js
@@ -9,7 +9,17 @@ router.get('/', async (req, res, next) => {
         // explicitly select only the id and email fields - even though
         // users' passwords are encrypted, it won't help if we just
         // send everything to anyone who asks!
-        attributes: ['id', 'name', 'position', 'imageUrl', 'industry', 'blurb']
+        attributes: [
+          'id',
+          'firstName',
+          'lastName',
+          'imgUrl',
+          'gender',
+          'company',
+          'position',
+          'dateJoinedCompany',
+          'blurb'
+        ]
       },
       {
         include: [
