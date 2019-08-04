8fb84807867c2669ceb62059f54d9aa833cee534
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-04 10:42:30 -0400

    chore: removed some regular auth functionalities

--- server/auth/google.js
+++ server/auth/google.js
@@ -30,6 +30,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   const strategy = new GoogleStrategy(
     googleConfig,
     (token, refreshToken, profile, done) => {
+      console.log('profile: >>>>>>>>>>>>>>>', profile)
       const googleId = profile.id
       const name = profile.displayName
       const email = profile.emails[0].value
