0ed53a20bd7ebf9eec93a0598d5d7714f4052c39
Macarena Carreno <macarenacarreno@Macarenas-MacBook-Pro.local>
2019-08-06 14:24:50 -0400

    Adding routes for search skills

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {Search} from './search'
 
 /**
  * COMPONENT
@@ -11,6 +12,7 @@ export const UserHome = props => {
   return (
     <div>
       <h3>Welcome, {firstName ? firstName : email}.</h3>
+      <Search />
     </div>
   )
 }
