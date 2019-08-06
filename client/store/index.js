2ba7342b8919d1ae11ff08aef12d3e46af94887b
Linda Saraguro <lindasaraguro@yahoo.com>
2019-08-06 10:54:34 -0400

    feat: add mentor subReducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import mentor from './mentor'
+import mentee from './mentee'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, mentor, mentee})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +14,5 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './mentor'
+export * from './mentee'
