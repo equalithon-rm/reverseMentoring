ecec607d9742425dd6fdbfe7b6c5dba56dbbe50a
Macarena Carreno <macarenacarreno@Macarenas-MacBook-Pro.local>
2019-08-06 11:48:11 -0400

    refactor: change the connection in models removing the idMentor & idmentee in skill table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,7 @@ const User = require('./user')
 const Mentee = require('./mentee')
 const Mentor = require('./mentor')
 const Skill = require('./skill')
+const Booking = require('./booking')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -17,18 +18,18 @@ const Skill = require('./skill')
  * instead of: const User = require('../db/models/user')
  */
 
+/// MENTOR ///
 Mentor.belongsToMany(Mentee, {through: 'mentors_mentees'})
+Mentee.hasMany(Booking)
+
+// MENTEE ///
 Mentee.belongsToMany(Mentor, {through: 'mentors_mentees'})
-
-Mentor.hasMany(Skill)
-Skill.belongsTo(Mentor)
-
-Mentee.hasMany(Skill)
-Skill.belongsTo(Mentee)
+Mentor.hasMany(Booking)
 
 module.exports = {
   User,
   Mentee,
   Mentor,
-  Skill
+  Skill,
+  Booking
 }
