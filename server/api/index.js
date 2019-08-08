438d265d7de365c042c98bc6c925768ff5900ef4
Linda Saraguro <lindasaraguro@yahoo.com>
2019-08-08 11:11:18 -0400

    feat: added booking subReducer

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/mentees', require('./mentees'))
 router.use('/mentors', require('./mentors'))
+router.use('./bookings', require('./bookings'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
