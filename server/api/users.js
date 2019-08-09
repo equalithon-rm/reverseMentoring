43b634b9bca667abc29e28879f3793bdd54b0231
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-09 13:55:04 -0400

    chore: commented out skill related code in users routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User, Skill} = require('../db/models')
+const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -19,15 +19,15 @@ router.get('/', async (req, res, next) => {
           'dateJoinedCurrentCompany',
           'bio'
         ]
-      },
-      {
-        include: [
-          {
-            model: Skill,
-            attributes: ['name']
-          }
-        ]
       }
+      // {
+      //   include: [
+      //     {
+      //       model: Skill,
+      //       attributes: ['name']
+      //     }
+      //   ]
+      // }
     )
     res.json(users)
   } catch (err) {
@@ -35,52 +35,52 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const users = await User.findOne(
-      {
-        where: {
-          id: req.params.id
-        }
-      },
-      {
-        include: [
-          {
-            model: Skill,
-            attributes: ['name'],
-            where: {
-              userId: req.params.id
-            }
-          }
-        ]
-      }
-    )
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     const users = await User.findOne(
+//       {
+//         where: {
+//           id: req.params.id
+//         }
+//       },
+//       {
+//         include: [
+//           {
+//             model: Skill,
+//             attributes: ['name'],
+//             where: {
+//               userId: req.params.id
+//             }
+//           }
+//         ]
+//       }
+//     )
+//     res.json(users)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
-router.put('/:id', async (req, res, next) => {
-  const {gender, currentCompany, currentPosition, bio} = req.body
-  const userId = req.params.id
-  try {
-    const [numberOfAffectedRows, userInstance] = await User.update(
-      {
-        gender: gender,
-        currentCompany: currentCompany,
-        currentPosition: currentPosition,
-        bio: bio,
-        hasCompletedSignup: true
-      },
-      {
-        where: {id: userId},
-        returning: true,
-        plain: true
-      }
-    )
-    res.json(userInstance)
-  } catch (err) {
-    next(err)
-  }
-})
+// router.put('/:id', async (req, res, next) => {
+//   const {gender, currentCompany, currentPosition, bio} = req.body
+//   const userId = req.params.id
+//   try {
+//     const [numberOfAffectedRows, userInstance] = await User.update(
+//       {
+//         gender: gender,
+//         currentCompany: currentCompany,
+//         currentPosition: currentPosition,
+//         bio: bio,
+//         hasCompletedSignup: true
+//       },
+//       {
+//         where: {id: userId},
+//         returning: true,
+//         plain: true
+//       }
+//     )
+//     res.json(userInstance)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
