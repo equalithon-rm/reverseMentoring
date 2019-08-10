5a40a5147bc7ca793b1b9793edd5860e17f4b2bf
Linda Saraguro <lindasaraguro@yahoo.com>
2019-08-10 00:35:53 -0400

    fix: updates to dashboard component

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,22 +1,28 @@
 import React, {Component} from 'react'
-// import {skills} from '../store/skillReducer'
+import {currentSkill} from '../store/skillsListReducer'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 
 export class Dashboard extends Component {
-  async componentDidMount() {}
+  componentDidMount() {
+    //trouble grabbing the desired skill Id, so hardcoded a desired skillId
+    this.props.currentSkill(1)
+    // grab all users with current skill
+  }
 
   render() {
-    // const {} = this.props
+    const {usersWithCurrentSkill} = this.props
+    console.log('INFO', usersWithCurrentSkill)
 
     return (
       <div id="dashbaord-list">
         <h1>Dashboard List</h1>
-        {/* {mentees.map(mentee => (
-          <div key={mentee.id}>
-            <h2>{`${mentee.firstName} ${mentee.lastName}`}</h2>
-            <p>{mentee.blurb}</p>
+        {/* {usersWithCurrentSkill.map(user => (
+          <div key={user.id}>
+          <img src={user.imgUrl}/>78tuyl.;..
+            <h2>{`${user.firstName} ${user.lastName}`}</h2>
+            <p>{user.bio}</p>
           </div>
         ))} */}
       </div>
@@ -25,11 +31,18 @@ export class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
-  return {}
+  // console.log("USER STATE", state.user)
+  // console.log("SKILL STATE", state.skillsReducer)
+  return {
+    userId: state.user.id,
+    usersWithCurrentSkill: state.skillsReducer.usersWithCurrentSkill
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {}
+  return {
+    currentSkill: skillId => dispatch(currentSkill(skillId))
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- client/store/skillsListReducer.js
+++ client/store/skillsListReducer.js
@@ -3,10 +3,15 @@ import axios from 'axios'
 // ACTION TYPES //
 
 const GET_SKILLS = 'GET_SKILLS'
+const GET_USERS_WITH_CURRENT_SKILL = 'GET_USERS_WITH_CURRENT_SKILL'
 
 // ACTION CREATORS //
 
 const getSkills = skill => ({type: GET_SKILLS, skill})
+const getUsersWithCurrentSkill = skillId => ({
+  type: GET_USERS_WITH_CURRENT_SKILL,
+  skillId
+})
 
 //Think Creators//
 
@@ -20,10 +25,21 @@ export const skills = () => async dispatch => {
   }
 }
 
+export const currentSkill = skillId => async dispatch => {
+  try {
+    const res = await axios.get(`/skills/currentSkills/${skillId}`)
+    console.log('THUNK DATA', res.data)
+    dispatch(getUsersWithCurrentSkill(res.data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 //INIITIAL STATE //
 
 const initialState = {
-  skills: []
+  skills: [],
+  usersWithCurrentSkill: []
 }
 
 // REDUCER //
@@ -32,6 +48,8 @@ const skillsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_SKILLS:
       return {...state, skills: action.skills}
+    case GET_USERS_WITH_CURRENT_SKILL:
+      return {...state, usersWithCurrentSkill: action.skillId.users}
     default:
       return state
   }
