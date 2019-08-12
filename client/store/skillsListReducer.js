ce442e98bb86630460d8ce38cd69d606aee9357c
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-12 01:00:20 -0400

    feat: added skillsHas and skillsWants thunk creators

--- client/store/skillsListReducer.js
+++ client/store/skillsListReducer.js
@@ -4,17 +4,50 @@ import axios from 'axios'
 
 const GET_SKILLS = 'GET_SKILLS'
 
+const GOT_SKILLS_USER_HAS = 'GOT_SKILLS_USER_HAS'
+const GOT_SKILLS_USER_WANTS = 'GOT_SKILLS_USER_WANTS'
+
 // ACTION CREATORS //
 
 const getSkills = skill => ({type: GET_SKILLS, skill})
 
-//Think Creators//
+const gotSkillsUserHasActionCreator = skills => ({
+  type: GOT_SKILLS_USER_HAS,
+  skills
+})
+
+const gotSkillsUserWantsActionCreator = skills => ({
+  type: GOT_SKILLS_USER_WANTS,
+  skills
+})
+
+//THUNK CREATORS//
 
 export const skills = () => async dispatch => {
   try {
-    const res = await axios.get('/skills/')
+    const res = await axios.get('/api/skills')
     console.log('rest in reducer', res)
     dispatch(getSkills(res.data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getSkillsUserHasThunkCreator = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/skills/currentSkills')
+    console.log('skillsUserHas data: ', data)
+    dispatch(gotSkillsUserHasActionCreator(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getSkillsUserWantsThunkCreator = () => async dispatch => {
+  try {
+    const {data} = await axios.get('api/skills/skillsInterestedIn')
+    console.log('skillsUserHas data: ', data)
+    dispatch(gotSkillsUserWantsActionCreator(data))
   } catch (error) {
     console.error(error)
   }
