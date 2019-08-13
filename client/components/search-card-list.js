9205937c0afaeb6edb2e7fee524334d26442b029
sam-peach <sampeachart@gmail.com>
2019-08-13 17:13:55 -0400

    feat: 'send request' button is disabled after being clicked.

--- client/components/search-card-list.js
+++ client/components/search-card-list.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import {Content, Media, Box} from 'react-bulma-components'
+import axios from 'axios'
 
 const handleClick = async (email, targetName, userName) => {
   await axios.post('/api/sendEmail', {email, targetName, userName})
@@ -19,17 +19,21 @@ const SearchCardList = props => {
             <h5>{curUser.user.currentCompany}</h5>
             <p>{curUser.user.currentPosition}</p>
             <p>{curUser.user.bio}</p>
-            <Link
-              onClick={() =>
+
+            <button
+              type="button"
+              onClick={evt => {
+                evt.target.disabled = true
+                evt.target.innerHTML = 'Request sent'
                 handleClick(
                   curUser.user.email,
                   curUser.user.fullName,
                   currentUserName
                 )
-              }
+              }}
             >
-              <button type="button">Send a request to connect</button>
-            </Link>
+              Send a request to connect
+            </button>
           </Content>
         </Media.Item>
       </Media>
